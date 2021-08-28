16a32152c50b5585b87922d822285defd2c4a77b
Darshan Bhut <darshan.bhut@abhinavitsolutions.com>
2021-08-28 16:56:01 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+
+require("./models/User");
+require("./services/passport");
+
+mongoose
+  .connect(keys.mongoURI)
+  .then(() => {
+    console.log("Connected to database ");
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database. \n${err}`);
+  });
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({bye:'buddy'})
-})
+app.use(cookieSession({
+    maxAge: 30 *24 * 60 * 60 * 1000,
+    keys:[keys.cookieKey]
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRouts")(app);
 
 const PORT = process.env.PORT || 5000;
 
